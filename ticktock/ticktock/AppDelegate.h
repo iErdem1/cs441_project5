633f4e69db21aceedceaadf991e61d2391b2ec0d
Ahmed Ihsan Erdem <ahmedihsanerdem@gmail.com>
2019-04-05 21:52:12 -0400

    Project XCode Template added
    Signed-off-by: Ahmed Ihsan Erdem <ahmedihsanerdem@gmail.com>

---
+++ ticktock/ticktock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ticktock
+//
+//  Created by Erdem Meral on 4/5/19.
+//  Copyright © 2019 Ahmed Ihsan Erdem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ticktock/ticktock/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  ticktock
+//
+//  Created by Erdem Meral on 4/5/19.
+//  Copyright © 2019 Ahmed Ihsan Erdem. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
